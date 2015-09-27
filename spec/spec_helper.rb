973db69955c8e438bee4e10a4a6287e87817ee0d
Stan Low <a22301613@yahoo.com.tw>
2015-09-27 23:51:43 +0800

    Add test coverage setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require "safe_finder"
 require "pry"
 require "support/models"
